33240d04158714dd810f8953382652cd569bf9a4
naveenb13 <naveenb13598@gmail.com>
2022-10-19 16:04:11 +0530

    final commit

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ async function photos() {
     try {
         let searchValue = document.getElementById("value1").value;
         console.log(searchValue);
-        let res = await fetch(`https://imsea.herokuapp.com/api/1?q=${searchValue}`);
+        let res = await fetch(`https://api.disneyapi.dev/character?name=${searchValue}`);
         let res1 = await res.json();
         console.log(res1);
 
         document.getElementById("cardShow1").style.display = "flex";
-        document.getElementById("image1").innerHTML = `<img src=${res1.results[0]} alt=${res1.image_name} width="300px" height="400px" class="card-img-top img1" />`
-        document.getElementById("name1").innerText = `${res1.image_name}`
+        document.getElementById("cardShow2").style.display = "flex";
+        document.getElementById("name1").innerHTML = `<h1>${res1.data[0].name}</h1>`;
+        document.getElementById("image1").innerHTML = `<img src=${res1.data[0].imageUrl} alt=${res1.data[0].name} width="100px" height="300px" class="card-img-top img1" />`
+            document.getElementById("films1").innerText = `${res1.data[0].films}`;
+            document.getElementById("tvShows1").innerText = `${res1.data[0].tvShows}`;
+            document.getElementById("videoGames1").innerText = `${res1.data[0].videoGames}`;
 
     } catch (error) {
         console.log(error)
+        document.getElementById("cardShow1").style.display = "flex";
+        document.getElementById("cardShow2").style.display = "none";
+        document.getElementById("name1").innerHTML = `<h5>Your Character Search does not belong to the Family of Disney</h5>`;
+        document.getElementById("image1").innerHTML = `Nil`
     }
 
 }
